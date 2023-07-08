b3c5f3fb0de1f412de63c1809adc5ecb57db42d3
Ranyere <ranyereramires@gmail.com>
2023-07-08 13:37:11 -0300

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home-overview/home.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/caminho-da-pagina-inicial', pathMatch: 'full' },
-  { path: 'caminho-da-pagina-inicial', component: HomeComponent },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home-overview/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
